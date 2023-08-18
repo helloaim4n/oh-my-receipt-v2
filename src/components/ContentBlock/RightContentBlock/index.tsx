5423523e9fb10d6348637f9ca18b823ba4e29e70
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-08-19 00:58:13 +0800

    Update to display content section

--- src/components/ContentBlock/RightContentBlock/index.tsx
+++ src/components/ContentBlock/RightContentBlock/index.tsx
@@ -1,5 +1,4 @@
 import { Row, Col } from "antd";
-import { SvgIcon } from "../../../common/SvgIcon";
 import { Button } from "../../../common/Button";
 import { ContentBlockProps } from "../types";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -10,17 +9,19 @@ import {
   Content,
   ContentWrapper,
   ButtonWrapper,
+  ServiceWrapper,  // Added ServiceWrapper import
+  MinTitle,
+  MinPara,
 } from "./styles";
 
 const RightBlock = ({
   title,
   content,
   button,
-  icon,
+  section,  // Added section prop
   id,
 }: ContentBlockProps) => {
 
-  // Defining a function to scroll to a specific element on the page with a smooth animation
   const scrollTo = (id: string) => {
     const element = document.getElementById(id) as HTMLDivElement;
     element.scrollIntoView({
@@ -34,28 +35,35 @@ const RightBlock = ({
         <Row justify="space-between" align="middle" id={id}>
           <Col lg={11} md={11} sm={11} xs={24}>
             <ContentWrapper>
-              <h6>{(title)}</h6>
-              <Content>{(content)}</Content>
+              <h6>{title}</h6>
+              <Content>{content}</Content>
               <ButtonWrapper>
                 {typeof button === "object" &&
                   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                  button.map((item: any, id: number) => {
-                    return (
-                      <Button
-                        key={id}
-                        color={item.color}
-                        fixedWidth={true}
-                        onClick={() => scrollTo("about")}
-                      >
-                        {(item.title)}
-                      </Button>
-                    );
-                  })}
+                  button.map((item: any, id: number) => (
+                    <Button
+                      key={id}
+                      color={item.color}
+                      fixedWidth={true}
+                      onClick={() => scrollTo("about")}
+                    >
+                      {item.title}
+                    </Button>
+                  ))}
               </ButtonWrapper>
+              <ServiceWrapper>  {/* Add ServiceWrapper */}
+                <Row justify="space-between">
+                  {typeof section === "object" &&
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    section.map((item: any, id: number) => (
+                      <Col key={id} span={11}>
+                        <MinTitle>{item.title}</MinTitle>
+                        <MinPara>{item.content}</MinPara>
+                      </Col>
+                    ))}
+                </Row>
+              </ServiceWrapper>
             </ContentWrapper>
-          </Col>
-          <Col lg={11} md={11} sm={12} xs={24}>
-            <SvgIcon src={icon} width="100%" height="100%" />
           </Col>
         </Row>
       </Fade>
