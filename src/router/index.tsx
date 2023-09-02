ebd02f7582529712e48d05d4f4987c530df01a88
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2023-09-03 03:14:22 +0800

    Refactor code for performance

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,43 +1,41 @@
-import  { lazy, Suspense } from "react";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { lazy, Suspense, useMemo } from "react";
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
 import routes from "./config";
 import { Styles } from "../styles/styles";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import NotFound from "../pages/NotFound";
 
 const Router = () => {
-  const location = useLocation();
-  const hideHeader =
-    location.pathname === "/privacy-policy" ||
-    location.pathname === "/terms-and-conditions";
+  const { pathname } = useLocation();
+  const isNotFoundPage = useMemo(() => pathname === "/404", [pathname]);
+  
+  const routeComponents = useMemo(() => routes.map((routeItem) => {
+    const PageComponent = lazy(() => import(`../pages/${routeItem.component}`));
+    const paths = Array.isArray(routeItem.path) ? routeItem.path : [routeItem.path];
+
+    return paths.map((path) => (
+      <Route key={`${routeItem.component}${path}`} path={path} element={<PageComponent />} />
+    ));
+  }), []);
 
   return (
-    <Suspense
-      fallback={null}>
+    isNotFoundPage ? 
+    <NotFound /> :
+    <Suspense fallback={null}>
       <Styles />
-      {!hideHeader && <Header />}
-      <Routes>
-        {routes.map((routeItem) => {
-          const PageComponent = lazy(() =>
-            import(`../pages/${routeItem.component}`)
-          );
-
-          const paths = Array.isArray(routeItem.path)
-            ? routeItem.path
-            : [routeItem.path];
-
-          return paths.map((path) => (
-            <Route
-              key={routeItem.component + path}
-              path={path}
-              element={<PageComponent />}
-            />
-          ));
-        })}
-      </Routes>
+      <Header />
+        <Routes>
+          <Route path="*" element={<Navigate to="/404" />} /> {/* Redirect to 404 page when user enter wrong URL */}
+          {routeComponents}
+        </Routes>
       <Footer />
     </Suspense>
   );
 };
 
 export default Router;
+
+
+
+
