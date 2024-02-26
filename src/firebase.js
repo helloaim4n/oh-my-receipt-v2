9d486bfcad77e051f366187ece97d7cf22e8eb08
Aiman Aizuddin <aimanaizuddin30@gmail.com>
2024-02-27 01:34:54 +0800

    Update Firebase configuration with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3xtbCczmeDvErZfkvWwyuI_GuFl3TC4U",
-  authDomain: "oh-my-receipt-75edb.firebaseapp.com",
-  projectId: "oh-my-receipt-75edb",
-  storageBucket: "oh-my-receipt-75edb.appspot.com",
-  messagingSenderId: "281427518131",
-  appId: "1:281427518131:web:55e0e2791cb250a50ec6c6",
-  measurementId: "G-M4733BZ507",
+  apiKey: import.meta.env.REACT_APP_API_KEY,
+  authDomain: import.meta.env.REACT_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.REACT_APP_PROJECT_ID,
+  storageBucket: import.meta.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.REACT_APP_APP_ID,
+  measurementId: import.meta.env.REACT_APP_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
